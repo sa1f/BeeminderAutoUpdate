930d09c5dfb804c838bf79ab364081717bb8e36b
Saif Sajid <saifmsajid@gmail.com>
2015-12-26 08:21:31 -0800

    Updated readme, ignored pyc

--- Beeminder.py
+++ Beeminder.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
 
 from FatSecret import *
-print calories
+
+# Check for beeminder_info.json, else get auth_token
+
+if not os.path.isfile('beeminder_info.json'):
+    print "Sign into beeminder and visit: https://www.beeminder.com/api/v1/auth_token.json"
+    info['username'] = raw_input("Enter your username: ")
+    info['auth_token'] = raw_input("Enter your auth_token code: ")
+    info['weight_goal_name'] = raw_input("Enter your weight goal name: ")
+    info['calorie_goal_name'] = raw_input("Enter your calorie goal name: ")
+    with open('beeminder_info.json', 'w') as outfile:
+        json.dump(info, outfile)
+
+
+with open('beeminder_info.json', 'r') as infile:
+    info = json.load(infile)
+
+params = {'value':weight, 'auth_token': info['auth_token']}
+baseUrl = "https://www.beeminder.com/api/v1/" + "users/" + info['username']
+r = requests.post(baseUrl + "/goals/" + info['weight_goal_name'] + "/datapoints.json", params=params)
+
+params = {'value':calories, 'auth_token': info['auth_token']}
+baseUrl = "https://www.beeminder.com/api/v1/" + "users/" + info['username']
+r = requests.post(baseUrl + "/goals/" + info['calorie_goal_name'] + "/datapoints.json", params=params)
+
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # FatSecret API
 
-My first project working with REST and Oauth. Just want to get my calorie and weight data from FatSecret to Beeminder
+My first project working with REST and Oauth. Just want to get my calorie and weight data from FatSecret into Beeminder
+
+
+**Requires:** 
+* rauth 
+* requests 
+
+**Instructions:**
+
+1. Install required modules using pip
+2. Get [FatSecret keys](http://platform.fatsecret.com)
+3. Run Beeminder.py
+4. For the Beeminder username and goal names, here's the format from one of the goal urls:
+    https://www.beeminder.com/{username}/goals/{goalName}
+5. Go ahead and automate the script using crontab
